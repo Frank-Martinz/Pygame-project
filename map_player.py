04f25a4cb24ccdb75404293665dc5bdad1ca6125
Dmitrii <andreanov1234@gmail.com>
2023-01-13 19:35:19 +1000

    Доработан персонаж

--- map_player.py
+++ map_player.py
@@ -2,11 +2,11 @@ import pygame
 import os
 import sys
 
-
 from pygame import *
 
 PLATFORM_WIDTH = 60
 PLATFORM_HEIGHT = 32
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -17,29 +17,13 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
-
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(all_sprites, player_sp)
         self.image = load_image('sage-frame-0.png')
         self.rect = self.image.get_rect()
 
-        self.rect.x = 10
+        self.rect.x = 300
         self.rect.y = 200
 
         self.player_has_jumped = False
@@ -54,11 +38,27 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         if not self.player_on_the_ground:
             self.rect.y += self.falling_speed
-            self.falling_speed += 0.2
+            if self.falling_speed != 7:
+                self.falling_speed += 0.2
             if self.rect.y + self.rect.height >= 400:
                 self.falling_speed = 0
                 self.player_has_jumped = False
                 self.player_on_the_ground = True
+
+        if self.rect.y + self.rect.height >= 400:
+            self.player_on_the_ground = True
+            self.player_has_jumped = False
+
+        else:
+            self.player_on_the_ground = False
+            self.player_has_jumped = True
+
+        if pygame.sprite.spritecollideany(self, objects_ps):
+            for obj in pygame.sprite.spritecollide(self, objects_ps, dokill=False):
+                if self.rect.bottom <= obj.rect.top + 9:
+                    self.player_on_the_ground = True
+                    self.player_has_jumped = False
+                    self.falling_speed = 0
 
         if pygame.sprite.spritecollideany(self, enemies_sp):
             for enm in enemies_sp:
@@ -67,7 +67,7 @@ class Player(pygame.sprite.Sprite):
                     self.falling_speed = -4
                 else:
                     self.death()
-
+                    return
 
     def death(self):
         self.kill()
@@ -75,21 +75,37 @@ class Player(pygame.sprite.Sprite):
     def move(self, dx):
         self.rect.x += dx
 
+    def can_make_right_move(self):
+        if pygame.sprite.spritecollideany(self, objects_ps):
+            for sp in pygame.sprite.spritecollide(self, objects_ps, dokill=False):
+                if sp.rect.top + 9 <= self.rect.bottom:
+                    if self.rect.left < sp.rect.right - 3:
+                        return False
+        return True
+
+    def can_make_left_move(self):
+        if pygame.sprite.spritecollideany(self, objects_ps):
+            for sp in pygame.sprite.spritecollide(self, objects_ps, dokill=False):
+                if sp.rect.top + 9 <= self.rect.bottom:
+                    if sp.rect.left + 3 < self.rect.right:
+                        return False
+        return True
+
     def jump(self):
         if self.player_on_the_ground:
-            self.falling_speed = -7
+            self.falling_speed = -8
             self.player_has_jumped = True
             self.player_on_the_ground = False
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__(all_sprites, enemies_sp)
         self.image = load_image('enemy-frame-0.png')
         self.rect = self.image.get_rect()
 
-        self.rect.x = 300
-        self.rect.y = 200
+        self.rect.x = x
+        self.rect.y = y
 
         self.move = 50
         self.step = -1
@@ -121,6 +137,44 @@ class Enemy(pygame.sprite.Sprite):
         self.kill()
 
 
+class Objects(pygame.sprite.Sprite):
+    def __init__(self, image, x, y):
+        self.image = image
+        super().__init__(all_sprites, objects_ps)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+def load_level(level):
+    x = y = 0
+    for row in level:
+        for col in row:
+            if col == "+":
+                pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+                pf = image.load("objects/1.png")
+                Objects(pf, x, y + 45)
+
+            if col == "*":
+                pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+                pf = image.load("objects/2.png")
+                Objects(pf, x, y + 8)
+
+            if col == "-":
+                pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+                pf = image.load("objects/4.png")
+                Objects(pf, x, y)
+
+            if col == "@":
+                pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+                pf = image.load("objects/3.png")
+                Objects(pf, x, y + 20)
+
+            x += PLATFORM_WIDTH
+        y += PLATFORM_HEIGHT
+        x = 0
+
+
 if __name__ == '__main__':
     pygame.init()
     all_sprites = pygame.sprite.Group()
@@ -137,7 +191,7 @@ if __name__ == '__main__':
         "                                   ",
         "          *            -       -   ",
         "                                   ",
-        "     +          +     @            ",
+        "   +            +     @            ",
         "                                   ",
         "                                   ",
         "                                   ",
@@ -156,64 +210,41 @@ if __name__ == '__main__':
     all_sprites = pygame.sprite.Group()
     player_sp = pygame.sprite.Group()
     enemies_sp = pygame.sprite.Group()
+    objects_ps = pygame.sprite.Group()
 
     running = True
     clock = pygame.time.Clock()
 
     player = Player()
-    enemy = Enemy()
-    camera = Camera()
+    enemy = Enemy(10, 400)
+    load_level(level)
 
     while running:
-        camera.update(player)
-
         screen.fill((255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        x = y = 0
-        for row in level:
-            for col in row:
-                if col == "+":
-                    pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                    pf = image.load("objects/1.png")
-                    screen.blit(pf, (x, y))
 
-                if col == "*":
-                    pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                    pf = image.load("objects/2.png")
-                    screen.blit(pf, (x, y + 8))
-
-                if col == "-":
-                    pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                    pf = image.load("objects/4.png")
-                    screen.blit(pf, (x, y))
-
-                if col == "@":
-                    pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                    pf = image.load("objects/3.png")
-                    screen.blit(pf, (x, y - 20))
-
-                x += PLATFORM_WIDTH
-            y += PLATFORM_HEIGHT
-            x = 0
         keys = pygame.key.get_pressed()
         if any(keys):
             if keys[pygame.K_SPACE]:
                 player.jump()
 
             if keys[pygame.K_a]:
-                player.move(-3)
+                if player.can_make_left_move():
+                    for sp in all_sprites:
+                        sp.rect.x += 3
+                    player.move(-3)
 
             if keys[pygame.K_d]:
-                player.move(3)
-
-
+                if player.can_make_right_move():
+                    for sp in all_sprites:
+                        sp.rect.x -= 3
+                    player.move(3)
 
         all_sprites.update()
         all_sprites.draw(screen)
         pygame.display.update()
         clock.tick(FPS)
-
 
     pygame.quit()
